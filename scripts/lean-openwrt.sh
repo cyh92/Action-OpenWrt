e53a617f9116a69dc21af7f7c07eb04205a9e36e
筱筱细雨凉生颊 <cyh92@live.cn>
2026-04-11 11:51:51 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -117,6 +117,8 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-parentcontrol package/l
 git clone --depth=1 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
 #在线用户
 git clone --depth=1 https://gitcode.com/cyh92-dev/1925844031051067393.git package/luci-app-onlineusers
+#科学
+git clone --depth=1https://github.com/Openwrt-Passwall/openwrt-passwall2.git package/luci-app-passwall2
 
 # 智能组网
 git clone --depth=1 https://github.com/OrayOS/OpenOray.git package/OpenOray
